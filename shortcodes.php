5085932c294db051628573225b6de389ee58331c
tailoredweb <tailoredweb@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-02 09:50:05 +0000

    git-svn-id: https://plugins.svn.wordpress.org/tailored-tools/trunk@566366 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ shortcodes.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ *	Additional Shortcodes
+ */
+
+
+new TailoredTools_Shortcodes();
+
+
+class TailoredTools_Shortcodes {
+
+	function __construct() {
+		add_shortcode('tabs', array(&$this,'shortcode_ui_tabs'));
+		add_shortcode('pagecontent', array(&$this,'shortcode_pagecontent'));
+		add_filter('tailored_tools_mce_buttons', array(&$this,'add_mce_buttons'));
+	}
+	
+	
+	/**
+	 *	Register our buttons for TinyMCE to add our shortcode
+	 */
+	function add_mce_buttons($buttons) {
+		array_push($buttons, array(
+			'label'		=> 'Tabbed Content',
+			'shortcode'	=> "[tabs]<br /><h2>Tab Heading</h2><p>Content</p><h2>Tab Heading</h2><p>Content</p>[/tabs]",
+		));
+		array_push($buttons, array(
+			'label'		=> 'Include Content',
+			'shortcode'	=> "[pagecontent id='99']",
+		));
+		return $buttons;
+	}
+	
+	
+	/**
+	 *	Shortcode:  [tabs] for jQuery UI Tabs
+	 *	Javascript does the heavy lifting
+	 */
+	function shortcode_ui_tabs($atts=false, $content=null) {
+		// Strip start and end <p> tags to avoid broken HTML
+		if (substr($content, 0, 4)=='</p>')	$content = substr($content, 4);
+		if (substr($content, -3, 3)=='<p>')	$content = substr($content, 0, -3);
+		$content = trim($content);
+		// Apply a wrapper for each panel
+		$content = str_replace('<h2>', '</div><div class="tab_panel"><h2>', $content);
+		// Fix start and end <div> panels to avoid broken HTML
+		if (substr($content, 0, 6)=='</div>')	$content = substr($content, 6);
+		$content .= '</div>'."\n";
+		// Using do_shortcode() to apply shortcodes inside the tabs
+		$content = '<div class="ui_tabs">'."\n".do_shortcode($content)."\n".'</div>'."\n";
+		return $content;
+	}
+	
+
+	/**
+	 *	Shortcode: [pagecontent] to include content from another page
+	 *	Used for templates & includes. Optionally include heading too.
+	 *	Usage:  [pagecontent id="99" include_title="yes"]
+	 */
+	function shortcode_pagecontent($atts=false) {
+		$atts = shortcode_atts(array(
+			'id'			=> false,
+			'include_title'	=> (strtoupper($atts['include_title'])=='YES') ? true : false,
+		), $atts);
+		if (!is_numeric($atts['id']))	return '';
+		$page = get_page($atts['id']);
+		if (!$page)						return '';
+		ob_start();
+		echo apply_filters('the_content', $page->post_content);
+		return ob_get_clean();
+	}
+
+}
+
+
+?>
---
+++ tools.php
@@ -0,0 +1,33 @@
+<?php
+/*
+Plugin Name:	Tailored Tools
+Description:	Adds some functionality to WordPress that you'll need.
+Version:		1.3.2
+Author:			Tailored Web Services
+Author URI:		http://www.tailored.com.au
+*/
+
+
+
+
+//	Include Helper Classes
+if (!class_exists('TailoredTinyMCE'))			require( dirname(__FILE__).'/lib/tinymce.php' );
+if (!class_exists('TailoredForm'))				require( dirname(__FILE__).'/lib/class.forms.php' );
+if (!class_exists('TailoredTools_Shortcodes'))	require( dirname(__FILE__).'/shortcodes.php' );
+
+// Anti-spam Modules
+if (!class_exists('Tailored_reCAPTCHA'))		require( dirname(__FILE__).'/lib/class.recaptcha.php' );
+if (!class_exists('Tailored_Akismet'))			require( dirname(__FILE__).'/lib/class.akismet.php' );
+if (!class_exists('Tailored_Tools_AYAH'))		require( dirname(__FILE__).'/lib/class.ayah.php' );
+
+//	Contact Form
+if (!class_exists('ContactForm'))		require( dirname(__FILE__).'/form.contact.php' );
+
+//	Sample Form
+//if (!class_exists('SampleForm')		require( dirname(__FILE__).'/form.sample.php' );
+
+
+
+
+
+?>
